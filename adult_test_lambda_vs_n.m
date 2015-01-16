c612eeb23e88dfe7c2b8a09e23eccffe12f970b7
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-01-16 17:07:25 +0100

    lambda vs n experiment modified

--- adult_test_lambda_vs_n.m
+++ adult_test_lambda_vs_n.m
@@ -7,30 +7,37 @@ clearAllButBP;
 resdir = 'results';
 mkdir(resdir);
 
-nRange = 500:500:7000;
-lambdaStarArray = [];
+nRange = 50:50:2000;
+numRep = 100;
+lambdaStarMat = zeros(numRep,size(nRange,2));
 
 for k = 1:size(nRange,2)
+    for rep = 1:numRep
+        %% Dataset initialization
 
-%% Dataset initialization
+        % Load small dataset
+        ds = Adult(nRange(k),16282,'plusMinusOne');
 
-% Load small dataset
-ds = Adult(nRange(k),16282,'plusMinusOne');
+        %% Experiment 1 setup, laplace kernel
 
-%% Experiment 1 setup, laplace kernel
+        ker = @laplaceKernel;
+        fil = @tikhonov;
 
-ker = @laplaceKernel;
-fil = @tikhonov;
+        alg = krls(ker, fil,  1, 100);
 
-alg = krls(ker, fil,  1, 100);
+        exp = experiment(alg , ds , 1 , true , true , '' , resdir);
 
-exp = experiment(alg , ds , 1 , true , true , '' , resdir);
+        exp.run();
 
-exp.run();
-
-lambdaStarArray = [lambdaStarArray exp.result.filterParStar];
-
+        lambdaStarMat(rep,k) = exp.result.filterParStar;
+    end
 end
 
+%% Plot results
 % Plot lambda star as a function of n
-semilogy(nRange,lambdaStarArray);
+semilogy(nRange,mean(lambdaStarMat,1));
+
+%% Boxplot
+figure
+boxplot(lambdaStarArray)
+ set(gca,'YScale','log')
