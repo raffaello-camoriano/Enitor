92741cf8efa2c51f1d1fe354db6365735a75c3a8
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-01-30 12:38:38 +0100

    pumadyn32nh nystrom experiment script added

---
+++ scripts/nystrom_reg_path/pumadyn_test_nystrom_m_vs_lambda.m
@@ -0,0 +1,72 @@
+setenv('LC_ALL','C');
+addpath(genpath('.'));
+ 
+clearAllButBP;
+
+% Set experimental results relative directory name
+resdir = 'results';
+mkdir(resdir);
+
+%% Dataset initialization
+
+% Load dataset
+ds  = pumadyn(4096,4096, 32 , 'n' , 'h');
+
+% Fixed parameters
+fixedsigma = 3.4530;
+
+% Set range of m
+mRange = 1:50:2000;
+nM = size(mRange,2);
+
+% Set range of lambda
+lMin = -7;
+lMax = 2;
+nLambda = 20;
+lRange = logspace(lMin,lMax,nLambda);
+
+% Number of experiment repetitions for each parameter combination
+numRep = 6;
+
+testErr = zeros(nLambda, nM, numRep);
+
+for j = 1:size(lRange,2)
+    l = lRange(j);
+    
+    for k = 1:size(mRange,2)
+        m = mRange(k);
+
+        k
+
+        tmp = [];
+        for rep = 1:numRep
+
+            rep
+
+            map = @nystromUniform;
+            fil = @tikhonov;
+
+            alg = nrls(map , 1000 , fil,  1 , 1 , m , fixedsigma , l , 0);
+
+            exp = experiment(alg , ds , 1 , true , false , '' , resdir , 0);
+            exp.run();
+
+            testErr(j,k,rep) = exp.result.perf;
+        end
+    end
+end
+
+%% Plot results
+
+testErrMed = median(testErr,3);
+
+% Median test error surface
+figure
+surf(mRange , lRange , testErrMed)
+set(gca,'XScale','lin')
+set(gca,'YScale','log')
+
+% Test error boxplot
+% figure
+% boxplot(testErr)
+%set(gca,'YScale','log')
