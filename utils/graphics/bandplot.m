24d8c4b0b6b9bd48bde572b06d47f21cf4325d9a
Raffaello Camoriano <raffaello.camoriano@iit.it>
2016-01-27 19:21:58 +0100

    x axis data specification added to bandplot

--- utils/graphics/bandplot.m
+++ utils/graphics/bandplot.m
@@ -1,8 +1,9 @@
-function [ hMean ] = bandplot( data , color , alpha , log , numSD)
+function [ hMean ] = bandplot( X , Y , color , alpha , log , numSD)
 %BANDPLOT Plots mean and standard deviation of the values contained in the
 %n x m 'data' matrix
 %   INPUT
-%   data: n x m matrix (n: number of repetitions; m: number of iterations)
+%   X: X axis data (independent variable)
+%   Y: n x m matrix (n: number of repetitions; m: number of iterations)
 %   color: color of the plot + fill (Standard MATLAB color name, e.g. 'blue' or 'red')
 %   alpha: transparency parameter
 %   log: 1 or 0
@@ -12,20 +13,24 @@ function [ hMean ] = bandplot( data , color , alpha , log , numSD)
 %   hMean: Returns the handle to the mean value plot
 %   hFill: Returns the handle to the fill plot
 
-    m = mean(data)';
-    sd = std(data,0)';
+    if isempty(X)
+        X = 1:size(Y,2);
+    end
+
+    m = mean(Y)';
+    sd = std(Y,0)';
     f = [ m'+numSD*sd' , flipdim(m'-numSD*sd',2)]; 
     hold on;
     if log == 1
-        hMean = semilogx(1:size(data,2) , m , 'Color' ,color , 'LineWidth',1);
-        hFill = fill([1:size(data,2) , size(data,2):-1:1] , f, color, ...
+        hMean = semilogx(X , m , 'Color' ,color , 'LineWidth',1);
+        hFill = fill([X , flip(X)] , f, color, ...
             'FaceAlpha', alpha,'LineStyle','none');
-        semilogx(1:size(data,2) , m , 'Color' , color , 'LineWidth',1);    
+        semilogx(X , m , 'Color' , color , 'LineWidth',1);    
     else
-        hMean = plot(1:size(data,2) , m ,'Color' ,  color , 'LineWidth',1);
-        hFill = fill([1:size(data,2) , size(data,2):-1:1] , f, color, ...
+        hMean = plot(X , m ,'Color' ,  color , 'LineWidth',1);
+        hFill = fill([X , flip(X)] , f, color, ...
             'FaceAlpha', alpha,'LineStyle','none');
-        plot(1:size(data,2) , m , 'Color' , color , 'LineWidth',1);    
+        plot(X , m , 'Color' , color , 'LineWidth',1);    
     end
 end
 
