d747b1b4b59972e2829785f8fc3c9d6600c4f7f7
Raffaello Camoriano <raffaello.camoriano@iit.it>
2016-02-10 03:49:04 +0100

    disambiguated in-script temp variable name

--- utils/graphics/saveAllFigs.m
+++ utils/graphics/saveAllFigs.m
@@ -1,9 +1,9 @@
 h = get(0,'children');
-for i=1:length(h)
-  saveas(h(i), [figsdir 'figure' num2str(i) '.fig']);
+for saveAllFigsIdx=1:length(h)
+  saveas(h(saveAllFigsIdx), [figsdir 'figure' num2str(saveAllFigsIdx) '.fig']);
   pause(0.1);
-  saveas(h(i), [figsdir 'figure' num2str(i) '.png']);
+  saveas(h(saveAllFigsIdx), [figsdir 'figure' num2str(saveAllFigsIdx) '.png']);
   pause(0.1);
-  saveas(h(i), [figsdir 'figure' num2str(i) '.eps']);
+  saveas(h(saveAllFigsIdx), [figsdir 'figure' num2str(saveAllFigsIdx) '.eps']);
   pause(0.1);
 end
